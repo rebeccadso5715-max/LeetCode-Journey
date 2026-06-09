7c46933ca3e2a26634e3068537fd4da4afcd41e5
Rebecca <rebeccadso5715@gmail.com>
2026-06-09 09:30:48 +0530

    Update Solution.py

--- LeetCode/278 - First Bad Version/Solution.py
+++ LeetCode/278 - First Bad Version/Solution.py
@@ -3,14 +3,10 @@ class Solution:
 
         left = 1
         right = n
-
         while left < right:
-
-            mid = (left + right) // 2
-
+            mid = (left + right) / 2
             if isBadVersion(mid):
                 right = mid
             else:
                 left = mid + 1
-
         return left
