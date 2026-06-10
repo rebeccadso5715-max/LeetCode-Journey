f95ac097c90fce5bf3bb513ce81f711a24ad05c9
Rebecca <rebeccadso5715@gmail.com>
2026-06-10 20:32:11 +0530

    Create Solution.py

---
+++ LeetCode/20 - Valid Parenthesis/Solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s):
+
+        stack = []
+
+        mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for ch in s:
+
+            if ch in mapping:
+
+                if not stack:
+                    return False
+
+                top = stack.pop()
+
+                if top != mapping[ch]:
+                    return False
+
+            else:
+                stack.append(ch)
+
+        return len(stack) == 0
