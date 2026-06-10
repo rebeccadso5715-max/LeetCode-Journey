fec7ce39b12936ad9c22f497344c44f84673bfc1
Rebecca <rebeccadso5715@gmail.com>
2026-06-10 22:55:30 +0530

    Create solution.py

---
+++ LeetCode/70 - Climbing Stairs/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n):
+
+        if n <= 2:
+            return n
+
+        return (
+            self.climbStairs(n - 1)
+            +
+            self.climbStairs(n - 2)
+        )
